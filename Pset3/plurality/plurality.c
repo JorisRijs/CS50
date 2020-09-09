f16949616b9aa096fd992e4637946629d8fdbc06
JorisRijs <joris@freya22.nl>
2020-09-09 20:17:56 +0000

    made some style fixes as well as added runoff

--- Pset3/plurality/plurality.c
+++ Pset3/plurality/plurality.c
@@ -68,9 +68,11 @@ bool vote(string name)
 {
     // TODO
     int candidate_amount = sizeof candidates;
-    for(int i = 0; i < candidate_count; i++){
+    for (int i = 0; i < candidate_count; i++)
+    {
         //printf("%s\n", candidates[i].name);
-        if(strcmp(candidates[i].name, name) == 0){
+        if (strcmp(candidates[i].name, name) == 0)
+        {
             candidates[i].votes += 1;
             //printf("Current voter has %i votes\n", candidates[i].votes);
             return true;
@@ -87,18 +89,23 @@ void print_winner(void)
     int num_cmp = candidates[0].votes;
     // TODO
     int equal_int = 0;
-    for(int i = 0; i < candidate_count -1; i++){
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
         //Last i elements are already in place
-        for(int j = 0; j < candidate_count - i - 1; j++){
-            if(candidates[j].votes > candidates[j+1].votes){
-                candidate temp = candidates[j+1];
-                candidates[j+1] = candidates[j];
+        for (int j = 0; j < candidate_count - i - 1; j++)
+        {
+            if (candidates[j].votes > candidates[j + 1].votes)
+            {
+                candidate temp = candidates[j + 1];
+                candidates[j + 1] = candidates[j];
                 candidates[j] = temp;
             }
         }
     }
-    for(int i = 0; i < candidate_count; i++){
-        if(candidates[i].votes != num_cmp){
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes != num_cmp)
+        {
             is_same = false;
         }
     }
@@ -106,18 +113,22 @@ void print_winner(void)
     //         printf("%i\n", candidates[i].votes);
     // }
 
-    if(!is_same){
-        
-        if(candidates[candidate_count -1].votes != candidates[candidate_count -2].votes){
+    if (!is_same)
+    {
+        if (candidates[candidate_count - 1].votes != candidates[candidate_count - 2].votes)
+        {
             printf("%s\n", candidates[candidate_count - 1].name);
-
         }
-        if(candidates[candidate_count -1].votes == candidates[candidate_count -2].votes){
-            printf("%s\n", candidates[candidate_count-1].name);
-            printf("%s\n", candidates[candidate_count-2].name);
+        if (candidates[candidate_count - 1].votes == candidates[candidate_count - 2].votes)
+        {
+            printf("%s\n", candidates[candidate_count - 1].name);
+            printf("%s\n", candidates[candidate_count - 2].name);
         }
-    }else{
-        for(int i = 0; i < candidate_count; i++){
+    }
+    else
+    {
+        for (int i = 0; i < candidate_count; i++)
+        {
             printf("%s\n", candidates[i].name);
         }
     }
